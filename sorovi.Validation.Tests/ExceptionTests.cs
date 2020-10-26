3f40981c2564d8cf9f03adcc28c1fdedb6188f61
Andreas Soroko <andreas.soroko@oerlikon.com>
2020-10-26 08:41:03 +0100

    test: add test for custom exception with member

--- sorovi.Validation.Tests/ExceptionTests.cs
+++ sorovi.Validation.Tests/ExceptionTests.cs
@@ -8,14 +8,35 @@ namespace sorovi.Validation.Tests
 {
     public class ExceptionTests
     {
+        private class TestClass
+        {
+            public string MyString { get; set; }
+        }
+
         [Test]
         public void Should_Be_Possible_To_Overide_Exception()
         {
             string test = null;
 
             Action a = () => ThrowOn(() => test)
-                .WithException((in string type, in string message, in string name, string value) => new TestException())
+                .WithException((in string type, in string message, in string name, object value) => new TestException())
                 .IfNull();
+
+            a.Should().Throw<TestException>();
+        }
+
+        [Test]
+        public void Should_Be_Possible_To_Overide_Exception_With_Member_Call()
+        {
+            var testClass = new TestClass
+            {
+                MyString = "test",
+            };
+
+            Action a = () => ThrowOn(() => testClass)
+                .WithException((in string type, in string message, in string name, object value) => new TestException())
+                .IfNull()
+                .Member(p => p.MyString, v => v.IfEqualsTo("test"));
 
             a.Should().Throw<TestException>();
         }
