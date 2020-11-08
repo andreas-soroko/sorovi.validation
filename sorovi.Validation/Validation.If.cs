2b9f9cb07422a0d9a804ce0a9dccb9bf3c476794
Andreas Soroko <andreas.soroko@gmail.com>
2020-11-08 10:58:45 +0100

    fix: pass memberName down to 'if' error message

--- sorovi.Validation/Validation.If.cs
+++ sorovi.Validation/Validation.If.cs
@@ -10,7 +10,7 @@ namespace sorovi.Validation
         public static ArgumentInfoBase<T, TEx> If<T, TEx>(this ArgumentInfoBase<T, TEx> arg, Predicate<T> predicate, in string type = ValidationType.If, in string message = null)
             where TEx : Delegate
         {
-            if (predicate(arg.Value)) { arg.ExceptionHandler(type, ErrorMessage.For(type, message, arg.Value)); }
+            if (predicate(arg.Value)) { arg.ExceptionHandler(type, ErrorMessage.For(type, message, arg.MemberName)); }
 
             return arg;
         }
@@ -19,7 +19,7 @@ namespace sorovi.Validation
         public static ArgumentInfoBase<T, TEx> IfNot<T, TEx>(this ArgumentInfoBase<T, TEx> arg, Predicate<T> predicate, in string type = ValidationType.IfNot, in string message = null)
             where TEx : Delegate
         {
-            if (!predicate(arg.Value)) { arg.ExceptionHandler(type, ErrorMessage.For(type, message, arg.Value)); }
+            if (!predicate(arg.Value)) { arg.ExceptionHandler(type, ErrorMessage.For(type, message, arg.MemberName)); }
 
             return arg;
         }
